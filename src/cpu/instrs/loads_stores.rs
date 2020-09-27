045de82ecf04f0265cfb92dfc69465cd24301b36
simuuz <simococo03@gmail.com>
2020-09-27 12:17:42 +0200

    added ld.h

--- src/cpu/instrs/loads_stores.rs
+++ src/cpu/instrs/loads_stores.rs
@@ -9,6 +9,18 @@ impl Cpu {
     //  5 cycles	When used in an isolated context.
     // Flags affected: none
     // Opcode: 0b000110
+    pub fn in_byte(&mut self, bus: &Bus, instr: u16) {
+        
+    }
+
+    pub fn in_halfword(&mut self, bus: &Bus, instr: u16) {
+        
+    }
+
+    pub fn in_word(&mut self, bus: &Bus, instr: u16) {
+        
+    }
+
     pub fn ld_byte (&mut self, bus: &Bus, instr: u16) {
         let reg1_index = instr as usize & 0x1F;
         let reg2_index = (instr as usize >> 5) & 0x1F;
